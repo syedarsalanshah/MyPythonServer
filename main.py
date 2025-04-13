f87d1f2688e2b54c9101593667326d26a280a492
syedarsalanshah <syedarsalanshah02@gmail.com>
2025-04-13 17:21:13 +0500

    Your commit message here

--- main.py
+++ main.py
@@ -12,13 +12,17 @@ class User(BaseModel):
 @app.get("/users")
 def get_users():
     users = [
-        {"id": 1, "name": "Ali", "email": "ali@example.com"},
-        {"id": 2, "name": "Sara", "email": "sara@example.com"}
+        {"id": 1, "name": "Arsalan", "email": "Arsalan@example.com"},
+        {"id": 2, "name": "King", "email": "King@example.com"}
     ]
     return users
 
 @app.post("/users")
 def create_user(user: User):
     return user
+
+
+
+
 
 handler = Mangum(app)
